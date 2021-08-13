e083de000afb8176fb9baeaa6af15634f0cc9aa9
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-08-13 19:07:56 -0400

    adding dependencies, ask the first question of the user

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    /*
+    project title
+    Description
+    Table of Contents
+    Installation - installation instructions
+    Usage - usage information
+    License
+    Contributing - contribution guidelines
+    Tests - test instructions
+    Questions - contact me with questions:
+        GitHub user name and link to GitHub profile
+        email address
+    */
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter the title of your project.');
+                return false;
+            }
+        }
+    }
+];
+
+const promptProject = () => inquirer.prompt(questions);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log("going to create the file ", fileName, " with data:\n", data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptProject()
+    .then(projectData => {
+        return generateMarkdown(projectData);
+    })
+    .then(readmeMD => {
+        return writeToFile("./dist/README.md", readmeMD);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+;
+}
 
 // Function call to initialize app
 init();
+
